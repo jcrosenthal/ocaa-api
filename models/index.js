b6bdae7790c2e7e2eb7e6298d587c1e0c108613b
James Rosenthal <james@lovingly.com>
2019-12-14 01:34:46 -0500

    no message

--- models/index.js
+++ models/index.js
@@ -11,14 +11,32 @@ const db = {};
 let sequelize;
 
 // console.log(process.env);
+// /rds-ca-2019-root.pem
+// /rds-combined-ca-bundle.pem
+const rdsCa = fs.readFileSync(__dirname + '/rds-combined-ca-bundle.pem');
 
 sequelize = new Sequelize(config.database, config.username, config.password, Object.assign(config, {
   maxConcurrentQueries: 100,
   pool: {
     maxConnections: 5,
     maxIdleTime: 30
+  },
+  dialect: 'mysql',
+  "dialectOptions": {
+    "ssl": {
+      rejectUnauthorized: true,
+      ca: [rdsCa],
+      checkServerIdentity: (host, cert) => {
+        const error = tls.checkServerIdentity(host, cert);
+        if (error && !cert.subject.CN.endsWith('.rds.amazonaws.com')) {
+          return error;
+        }
+      }
+    }
   }
 }));
+
+console.log(sequelize);
 
 fs
   .readdirSync(__dirname)
